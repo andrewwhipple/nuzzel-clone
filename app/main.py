2464198b00487e6d2cdc55ebf42dff47cfccfb17
Andrew Whipple <android128@gmail.com>
2022-01-21 19:21:50 -0600

    get rid of the items route

--- app/main.py
+++ app/main.py
@@ -25,9 +25,6 @@ def get_db():
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
 
 @app.get("/users")
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):

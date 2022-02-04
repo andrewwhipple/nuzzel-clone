d3750da4f7a8f52b92b52f1d3a527a64e99f9b37
Andrew Whipple <android128@gmail.com>
2022-02-03 21:16:22 -0600

    worlds most basic fe with fake data

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,62 @@
+import React from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
+
+class LinkList extends React.Component {
+  render() {
+    let linksArray = [];
+    let sharers1 = ["Andrew", "Jane"]
+    let sharers2 = ["Bob"]
+    linksArray.push(<Link link="meow.com" sharers={sharers1}/>);
+    linksArray.push(<Link link="squirrel.edu" sharers={sharers2}/>);
+    linksArray.push(<Link link="nice.cool" sharers={sharers2}/>);
+
+    return (
+      <div>
+        {linksArray}
+      </div>
+    )
+  }
+}
+
+class Link extends React.Component {
+  render() {
+
+    let sharers = "";
+
+    for (var sharer in this.props.sharers) {
+      sharers = sharers + this.props.sharers[sharer];
+      if (sharer != this.props.sharers.length - 1) {
+          sharers = sharers + ", "
+      }
+    }
+
+
+
+    return (
+      <>
+      <p>A link has: </p>
+      <ul>
+        <li>The link url: <a href={this.props.link}>{this.props.link}</a></li>
+        <li>The number of people who shared it: {this.props.sharers.length}</li>
+        <li>The names of the people who shared it: {sharers}</li>  
+      </ul>
+      
+      
+      
+      </>
+    )
+  }
+
+}
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LinkList/>
   );
 }
 

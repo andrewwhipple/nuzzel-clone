66932720c0b820d32e8f15ee3633ddf1c4d2b4ba
Andrew Whipple <android128@gmail.com>
2022-02-06 13:14:05 -0600

    basic link list displaying

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,76 @@ import './App.css';
 
 
 class LinkList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: []
+    }
+
+    this.getLinks = this.getLinks.bind(this);
+
+  }
+
+  getLinks() {
+    fetch('/user/1/following_tweets?urls_only=true', {
+      method: 'GET',
+    }).then((res) => {
+      res.json().then((responseJson) => {
+        console.log(responseJson);
+        this.setState(
+          {
+            links: responseJson
+          }
+        )
+      })
+    }).catch(() => console.error('Error in fetching tweets'));
+  }
+  
+  
+  
   render() {
+    
+    //this.getLinks();
+
     let linksArray = [];
-    let sharers1 = ["Andrew", "Jane"]
-    let sharers2 = ["Bob"]
-    linksArray.push(<Link link="meow.com" sharers={sharers1}/>);
-    linksArray.push(<Link link="squirrel.edu" sharers={sharers2}/>);
-    linksArray.push(<Link link="nice.cool" sharers={sharers2}/>);
+    let linksDict = {};
+    for (let link in this.state.links) {
+      //console.log(this.state.links[link]);
+
+      if (this.state.links[link].url in linksDict) {
+        console.log(linksDict[this.state.links[link].url]);
+        let sharers = linksDict[this.state.links[link].url];
+        sharers.push(this.state.links[link].twitter_user_id)
+        linksDict[this.state.links[link].url] = sharers;
+
+      } else {
+        //console.log(this.state.links[link].url);
+        linksDict[this.state.links[link].url] = [this.state.links[link].twitter_user_id];
+      }
+      //console.log(linksDict);
+      
+
+      //check if the url is already in the links dictionary
+      //if yes:
+      //  add to sharers
+      //if no:
+      //  create a new link with sharers of the twitter_user_id for now, eventually connect to get the twitter user name from the background 
+
+    }
+
+    for (let new_link in linksDict) {
+      linksArray.push(<Link link={new_link} sharers={linksDict[new_link]}/>);
+    }
+
+
+
+
+    //let linksArray = [];
+    //let sharers1 = ["Andrew", "Jane"]
+    //let sharers2 = ["Bob"]
+    //linksArray.push(<Link link="meow.com" sharers={sharers1}/>);
+    //linksArray.push(<Link link="squirrel.edu" sharers={sharers2}/>);
+    //linksArray.push(<Link link="nice.cool" sharers={sharers2}/>);
 
     
 
@@ -20,6 +83,7 @@ class LinkList extends React.Component {
     return (
       <div>
         {linksArray}
+        <button type="button" onClick={this.getLinks}>Refresh</button>
       </div>
     )
   }
@@ -59,14 +123,7 @@ class Link extends React.Component {
 
 
 function App() {
-  
-  fetch('/user/1/following_tweets', {
-    method: 'GET',
-  }).then((res) => {
-    res.text().then((responseText) => {
-      console.log(responseText);
-    })
-  }).catch(() => console.error('Error in fetching tweets'));
+
   
   return (
     <LinkList/>

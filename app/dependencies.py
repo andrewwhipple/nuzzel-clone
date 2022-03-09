d1ac8cde3016a5fbfc3aadba09b577b7db20fda6
Andrew Whipple <android128@gmail.com>
2022-03-09 08:26:34 -0600

    populated dependencies file

---
+++ app/dependencies.py
@@ -0,0 +1,40 @@
+from app import models, schemas
+from app.database import SessionLocal, engine
+from sqlalchemy.orm import Session
+models.Base.metadata.create_all(bind=engine)
+
+from pydantic import BaseSettings
+from typing import Optional
+
+import tweepy
+
+class Settings(BaseSettings):
+    twitter_token: str
+    twitter_api_key: str
+    twitter_api_secret: str
+    twitter_access_token: str
+    twitter_access_token_secret: str
+    get_followers: bool = False
+    last_paginated_users_max: int = 0
+    page_size: int = 800
+    last_paginated_followers_max: int = 0
+    followers_page_size: int = 10
+
+settings = Settings()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+client = tweepy.Client(
+    bearer_token=settings.twitter_token,
+    consumer_key=settings.twitter_api_key,
+    consumer_secret=settings.twitter_api_secret,
+    access_token=settings.twitter_access_token,
+    access_token_secret=settings.twitter_access_token_secret
+)

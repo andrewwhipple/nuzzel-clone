70b4de4bd50dcaf8d5d1149f697132d09616af1b
Andrew Whipple <android128@gmail.com>
2022-06-06 08:26:36 -0400

    Added test for get users

---
+++ app/tests/test_users.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+
+import pytest
+from app.main import app as meow
+from app import crud
+
+client = TestClient(meow)
+
+
+
+def test_get_users():
+    mocked_users = [{
+        'id': 1,
+        'twitter_user_id': '12345'
+    }]
+    with patch('app.routes.users.crud.get_users', return_value = mocked_users):
+        response = client.get(url='/api/users/')
+        assert response.status_code == 200
+        assert response.json() == [{'id': 1, 'twitter_user_id': '12345'}]
+
+
+
+    
